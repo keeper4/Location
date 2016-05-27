3e2e9fcaf98c8ee89161665d332b8f432d859988
Oleksandr Chyzh <a.v.chyzh@gmail.com>
2016-05-27 19:46:24 +0300

    first commit

---
+++ Location/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Location
+//
+//  Created by Oleksandr Chyzh on 27/5/16.
+//  Copyright © 2016 Oleksandr Chyzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Location/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Location
+//
+//  Created by Oleksandr Chyzh on 27/5/16.
+//  Copyright © 2016 Oleksandr Chyzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
